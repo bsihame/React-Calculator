8a975d3973e2d0da5b5dd41487cbee2758ee02e2
bsihame <bsihame@hotmail.com>
2022-09-30 20:51:09 -0400

    Add basic frame

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     hello 🗺 
+    <>
+    <div className="calc">
+      <input type="text" placeholder='0' id="answer"/>
+      
+      <input type="button" value="9" className="button"/>
+      <input type="button" value="8" className="button"/>
+      <input type="button" value="7" className="button"/>
+      <input type="button" value="6" className="button"/>
+      <input type="button" value="5" className="button"/>
+      <input type="button" value="4" className="button"/>
+      <input type="button" value="3" className="button"/>
+      <input type="button" value="2" className="button"/>
+      <input type="button" value="1" className="button"/>
+
+      <input type="button" value="0" className="button"/>
+      <input type="button" value="+" className="button"/>
+      <input type="button" value="-" className="button"/>
+      <input type="button" value="/" className="button"/>
+      <input type="button" value="*" className="button"/>
+      <input type="button" value="%" className="button"/>
+      <input type="button" value="." className="button"/>
+
+      <input type="button" value="Clear" className="button button1"/>
+      <input type="button" value="=" className="button button1"/>
     </div>
+    </>
   );
 }
 
